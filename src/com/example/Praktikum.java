f8eb688a9262933ec3403ff66964b55dd7a26495
Sayid Umarsayidov <x7z3@mail.ru>
2022-05-30 14:08:43 +0300

    Review fix

--- src/com/example/Praktikum.java
+++ src/com/example/Praktikum.java
@@ -17,6 +17,6 @@ public class Praktikum {
     }
 
     public static boolean isLeapYear(int year) {
-        return (year % 4 == 0 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 }
